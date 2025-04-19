ced7df19671dda642f1dffef53a9c212d9a98811
Vedant7924 <shindevedant752@gmail.com>
2025-04-19 19:11:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {Provider} from 'react-redux';
-import { createStore, applyMiddleware, compose } from "redux";
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import reducers from "./redux/reducer";
-import thunk from "redux-thunk";
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const data ={
-  index:0
-}
-const datas = JSON.parse(localStorage.getItem("sidebar"));
-if(!datas){
-  localStorage.setItem("sidebar",JSON.stringify(data));
-}
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
-  </BrowserRouter>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config({path:"./config.env"});
+import Commonrouter from './router/common.js';
+import Userrouter from "./router/userrouter.js";
+import bodyParser from "body-parser";
+app.use(express.json({limit: '20mb'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use('/common',Commonrouter);
+app.use("/user" , Userrouter);
+const port = 8000;
+const db = process.env.DB;
+mongoose.connect(db).then(()=>{
+    console.log("success")
+}).catch((err)=>{
+    console.log(err)
+});
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
+app.listen(port,()=>{
+    console.log("started")
+})
